41c6887b8260140f4c9ef7de62001d3d419352b8
sungjune.p <egs060@gmail.com>
2019-06-11 13:35:14 +0900

    ChangeInput

--- demo.py
+++ demo.py
@@ -16,7 +16,8 @@ logging.basicConfig(level=logging.INFO)
 flags = tf.app.flags
 flags.DEFINE_string('exp_dir', 'aster/experiments/demo/',
                     'Directory containing config, training log and evaluations')
-flags.DEFINE_string('input_image', 'aster/data/demo.jpg', 'Demo image')
+#flags.DEFINE_string('input_image', 'aster/data/demo.jpg', 'Demo image')
+flags.DEFINE_string('data_dir', 'aster/data/test_images', 'Input Cropped Images')
 FLAGS = flags.FLAGS
 
 
@@ -68,25 +69,29 @@ def main(_):
     'rectified_images': predictions_dict['rectified_images'],
   }
 
-  with open(FLAGS.input_image, 'rb') as f:
-    input_image_str = f.read()
-
   with tf.Session() as sess:
     sess.run([
       tf.global_variables_initializer(),
       tf.local_variables_initializer(),
       tf.tables_initializer()])
     saver.restore(sess, checkpoint)
-    sess_outputs = sess.run(fetches, feed_dict={input_image_str_tensor: input_image_str})
 
-  print('Recognized text: {}'.format(sess_outputs['recognition_text'].decode('utf-8')))
-  
-  rectified_image = sess_outputs['rectified_images'][0]
-  rectified_image_pil = Image.fromarray((128 * (rectified_image + 1.0)).astype(np.uint8))
-  input_image_dir = os.path.dirname(FLAGS.input_image)
-  rectified_image_save_path = os.path.join(input_image_dir, 'rectified_image.jpg')
-  rectified_image_pil.save(rectified_image_save_path)
-  print('Rectified image saved to {}'.format(rectified_image_save_path))
+    image_list = os.listdir(FLAGS.data_dir)
+    for file in image_list:
+      with open(os.path.join(FLAGS.data_dir, file), 'rb') as f:
+        input_image_str = f.read()
+
+
+      sess_outputs = sess.run(fetches, feed_dict={input_image_str_tensor: input_image_str})
+
+      print('Recognized text for %s: {}'.format(sess_outputs['recognition_text'].decode('utf-8')) %file)
+
+      rectified_image = sess_outputs['rectified_images'][0]
+      rectified_image_pil = Image.fromarray((128 * (rectified_image + 1.0)).astype(np.uint8))
+      #input_image_dir = os.path.dirname(FLAGS.input_image)
+      rectified_image_save_path = os.path.join(FLAGS.data_dir, 'rectifed %s' %file)
+      rectified_image_pil.save(rectified_image_save_path)
+      print('Rectified image saved to {}'.format(rectified_image_save_path))
 
 if __name__ == '__main__':
   tf.app.run()
